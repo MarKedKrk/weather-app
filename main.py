5d2d2e0535aafa5bd267855df39d7bd2e1cf36ce
MarKedKrk <kendi@epoczta.pl>
2026-05-27 17:10:39 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from services.openweather_api import get_weather
+import time
+
+while True:
+    weather = get_weather()
+    print(weather)
+    time.sleep(10)
---
+++ utils/tools.py
@@ -0,0 +1,8 @@
+
+
+# KELVINY NA C
+to_celcious = lambda temp: round(temp - 273.15,2)
+
+
+# M/S NA KM/H
+to_kmh = lambda speed: round(speed * 3.6,2)
